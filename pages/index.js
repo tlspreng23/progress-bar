daa9f519fe99a0ea2240ff4f06c2b0e2c223978c
tlspreng23 <167548972+tlspreng23@users.noreply.github.com>
2025-08-06 23:58:19 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import ProjectProgressApp from "@/components/ProjectProgressApp";
+import ProjectProgressApp from "../components/ProjectProgressApp";
 
 export default function Home() {
   return (
